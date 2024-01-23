65c10fa737a628d6b76ca80c123a6d786679749a
ArhonPineda <aronp2000@gmail.com>
2024-01-23 00:33:36 -0600

    update responsive style

--- src/components/Stack.tsx
+++ src/components/Stack.tsx
@@ -1,22 +1,22 @@
 const Stack = () => {
   return (
     <>
-      <div id="my-stack" className="px-6 text-center h-screen flex flex-col gap-6 md:px-28 lg:px-40 xl:px-60">
+      <div id="my-stack" className="px-6 text-center h-min flex flex-col gap-6 md:px-28 lg:px-40 xl:px-60">
           <h1 className="font-bold text-4xl text-center py-6">
             My Stack
           </h1>
           <hr className=""/>
           <p className="text-center">These are some tools I often work with</p>
-          <div className="items-center h-full my-auto text-7xl flex flex-wrap justify-center gap-16 mx-auto p-6 md:text-8xl md:w-full">
-            <i className="glow fa-brands fa-html5 hover:text-orange-400 transition-colors ease-in" aria-hidden="true"></i>
-            <i className="glow fa-brands fa-css3 hover:text-blue-400 transition-colors ease-in" aria-hidden="true"></i>
-            <i className="glow fa-brands fa-js hover:text-yellow-400 transition-colors ease-in" aria-hidden="true"></i>
-            <i className="glow fa-brands fa-sass hover:text-pink-400 transition-colors ease-in" aria-hidden="true"></i>
-            <i className="glow fa-brands fa-bootstrap hover:text-purple-400 transition-colors ease-in" aria-hidden="true"></i>
-            <i className="glow fa-brands fa-react hover:text-cyan-400 transition-colors ease-in" aria-hidden="true"></i>
-            <i className="glow fa-brands fa-figma hover:text-slate-400 transition-colors ease-in" aria-hidden="true"></i>
-            <i className="glow fa-brands fa-node hover:text-green-400 transition-colors ease-in" aria-hidden="true"></i>
-            <i className="glow fa-brands fa-python hover:text-yellow-200 transition-colors ease-in" aria-hidden="true"></i>
+          <div className="items-center h-full my-auto text-6xl flex flex-wrap justify-around gap-20 mx-auto p-6 md:text-8xl md:w-full">
+            <i className="border fa-brands fa-html5 hover:text-orange-400 transition-colors ease-in" aria-hidden="true"></i>
+            <i className="border fa-brands fa-css3 hover:text-blue-400 transition-colors ease-in" aria-hidden="true"></i>
+            <i className="border fa-brands fa-js hover:text-yellow-400 transition-colors ease-in" aria-hidden="true"></i>
+            <i className="border fa-brands fa-sass hover:text-pink-400 transition-colors ease-in" aria-hidden="true"></i>
+            <i className="border fa-brands fa-bootstrap hover:text-purple-400 transition-colors ease-in" aria-hidden="true"></i>
+            <i className="border fa-brands fa-react hover:text-cyan-400 transition-colors ease-in" aria-hidden="true"></i>
+            <i className="border fa-brands fa-figma hover:text-slate-400 transition-colors ease-in" aria-hidden="true"></i>
+            <i className="border fa-brands fa-node hover:text-green-400 transition-colors ease-in" aria-hidden="true"></i>
+            <i className="border fa-brands fa-python hover:text-yellow-200 transition-colors ease-in" aria-hidden="true"></i>
           </div>
         </div>
     </>
