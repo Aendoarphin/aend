23b4ccbb9243ee0c41cf97f1d68799f6b83360ea
Arhon <aronp2000@gmail.com>
2024-05-17 00:31:02 -0500

    edit styling

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
-import { SiPython, SiQt, SiJavascript, SiNextdotjs, SiReact, SiMongodb, SiCss3, SiSass, SiAuth0, IconType, SiGithub } from "@icons-pack/react-simple-icons";
+import { SiPython, SiQt, SiJavascript, SiNextdotjs, SiReact, SiMongodb, SiCss3, SiSass, SiAuth0 } from "@icons-pack/react-simple-icons";
 
 export default function Projects() {
 
-	const projects: Record<string, any> = [
+	type Project = {
+		title: string;
+		stack: JSX.Element[];
+		description: string;
+		from: string;
+		to: string;
+		link: string;
+		media: string | string[];
+	}
+
+	const projects: Project[] = [
 		{
 			title: "Comps",
 			stack: [<SiPython className="text-yellow-300" />, <SiQt className="text-green-400" />],
-			description: "Price analytics tool for sold trading cards. Allows collectors to find effective price references and identify historical sales trends.",
+			description: "This desktop application retrieves and processes sold listings data from eBay, streamlining the process for collectors to access and isolate sold trading card listings. It enables collectors to identify fair prices for buying or selling trading cards in the eBay trading card market, providing valuable insights for making informed pricing decisions.",
 			from: "January 2023",
 			to: "April 2023",
 			link: "https://github.com/Aendoarphin/comps_v1",
@@ -16,7 +26,7 @@ export default function Projects() {
 		{
 			title: "ScrapHouse",
 			stack: [<SiJavascript className=" text-yellow-300" />, <SiCss3 className="text-blue-500" />, <SiSass className="text-pink-500" />, <SiNextdotjs />, <SiReact className="text-cyan-500" />, <SiMongodb className="text-green-500 bg-foreground rounded-full" />, <SiAuth0 className="text-orange-600" />],
-			description: "Web application for sharing mixed martial arts content. Provides information about rankings, news, stats, and events.",
+			description: "This web application is in continuous development to deliver fighter rankings, news, and statistical data across various MMA promotions. Planned features, including the incorporation of event schedules and fighter directory, are in the works as part of this ongoing project.",
 			from: "December 2023",
 			to: "Present",
 			link: "https://github.com/Aendoarphin/scraphousemma",
@@ -25,7 +35,7 @@ export default function Projects() {
 		{
 			title: "Tool Life Utility",
 			stack: [<SiPython className="text-yellow-300" />, <SiQt className="text-green-400" />],
-			description: "Desktop application that presents machine production data. Allows machinists to monitor multiple machines in one central location.",
+			description: "The application centralizes data display, enabling CNC machine operators to avoid the necessity of individually inspecting each machine throughout the shop, thus reducing the need for extensive physical movement.",
 			from: "August 2023",
 			to: "September 2023",
 			link: "https://github.com/Aendoarphin/pyforcam",
@@ -40,11 +50,11 @@ export default function Projects() {
 	};
 
 	return (
-		<div id="projects" className="flex mt-10 flex-col px-8 sm:max-w-[30em] sm:mx-auto md:justify-center lg:mx-80 h-screen">
+		<div id="projects" className="flex mt-10 flex-col px-8 sm:max-w-[30em] mx-auto justify-center md:max-w-[50em] lg:max-w-[54em] h-screen">
 			<h1 className="text-heading font-semibold">Projects</h1>
 			<hr className="mb-4" />
 			<div id="project-buttons" className="flex flex-nowrap">
-				{projects.map((project: Record<string, any>, index: number) => (
+				{projects.map((project: Project, index: number) => (
 					<button
 						type="button"
 						key={index}
@@ -59,13 +69,12 @@ export default function Projects() {
 			<div className=" flex flex-col gap-2">
 				<p>{projects[selected].description}</p>
 				<div className="text-xs font-light flex flex-nowrap justify-between">
-					<a href={projects[selected].link} target="_blank" className="underline">Repository</a>
+					<a href={projects[selected].link} target="_blank" className="underline">Go To Project</a>
 					<p>{projects[selected].from} - {projects[selected].to}</p>
 				</div>
-				<div className="flex flex-nowrap justify-start gap-2">{projects[selected]["stack"].map((icon: IconType) => (
+				<div className="flex flex-nowrap justify-start gap-2">{projects[selected]["stack"].map((icon: JSX.Element) => (
 					icon
-				))} <br />
-					{Array.isArray(projects[selected].media) ? projects[selected].media.map() : <img src={projects[selected].media} alt="media" className="rounded-md" />}
+				))} 
 				</div>
 			</div>
 		</div>
